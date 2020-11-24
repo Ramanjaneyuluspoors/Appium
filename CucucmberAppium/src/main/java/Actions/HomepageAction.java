e5274d6b569ae763e391080473b071d6be1f680a
unknown <ramanjaneyulu.nelampati@spoors.in>
2020-11-24 11:47:41 +0530

    homepagechanges

--- CucucmberAppium/src/main/java/Actions/HomepageAction.java
+++ CucucmberAppium/src/main/java/Actions/HomepageAction.java
@@ -1,6 +1,7 @@
 package Actions;
 
 import java.net.MalformedURLException;
+import java.text.ParseException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
@@ -20,7 +21,7 @@ public class HomepageAction {
 	}
 
 	// verify user sign-in and perform accordingly
-	public static void signInAction() throws MalformedURLException, InterruptedException {
+	public static void signInAction() throws MalformedURLException, InterruptedException, ParseException {
 		CommonUtils.waitForElementVisibility("//*[@resource-id='in.spoors.effortplus:id/startStopWorkSwitch']");
 		CommonUtils.SwitchStatus("startStopWorkSwitch");
 		if (CommonUtils.SwitchStatus("startStopWorkSwitch").contains("OFF")) {
@@ -32,34 +33,34 @@ public class HomepageAction {
 	}
 
 	//signin cases
-	public static void sign_in_Options() throws InterruptedException, MalformedURLException {
+	public static void sign_in_Options() throws InterruptedException, MalformedURLException, ParseException {
 		// sign-in form(if sign-in form exist then capturing location page will not
-		// display)
+		// display) 
 		CommonUtils.interruptSyncAndLetmeWork();
-		if (CommonUtils.getdriver().findElement(MobileBy.id("saveForm")).isDisplayed()) {
+		if (CommonUtils.getdriver().findElements(By.id("saveForm")).size() > 0) {
 			Forms.verifyFormPagesAndFill();
-		} else if (CommonUtils.getdriver().findElement(MobileBy.id("signInButton")).isDisplayed()) {
-			CommonUtils.getdriver().findElement(MobileBy.id("signInButton")).click();
-			if (CommonUtils.getdriver().findElement(By.id("heading")).isDisplayed()) {
+		} else if (CommonUtils.getdriver().findElements(By.id("signInButton")).size() > 0) {
+			CommonUtils.getdriver().findElement(By.id("signInButton")).click();
+			if (CommonUtils.getdriver().findElements(By.id("heading")).size() > 0) {
 				CommonUtils.getdriver().findElements(By.className("android.widget.Button")).get(0).click();
 			}
 			CommonUtils.interruptSyncAndLetmeWork();
-			if (CommonUtils.getdriver().findElement(MobileBy.id("saveForm")).isDisplayed()) {
+			if (CommonUtils.getdriver().findElements(By.id("saveForm")).size() > 0) {
 				Forms.verifyFormPagesAndFill();
 			}
-		} else if (CommonUtils.getdriver().findElement(By.id("heading")).isDisplayed()) {
+		} else if (CommonUtils.getdriver().findElements(By.id("heading")).size() > 0) {
 			CommonUtils.getdriver().findElements(By.className("android.widget.Button")).get(0).click();
-		} else if (CommonUtils.getdriver().findElement(MobileBy.id("button1")).isDisplayed()) {
-			CommonUtils.getdriver().findElement(MobileBy.id("button1")).click();
+		} else if (CommonUtils.getdriver().findElements(By.xpath("//*[@text='SIGN IN']")).size() > 0) {
+			CommonUtils.getdriver().findElement(By.xpath("//*[@text='SIGN IN']")).click();
 		} else if (CommonUtils.getdriver()
-				.findElement(
-						MobileBy.xpath("//*[@resource-id='com.android.permissioncontroller:id/permission_message']"))
-				.isDisplayed()) {
+				.findElements(By.xpath("//*[@resource-id='com.android.permissioncontroller:id/permission_message']"))
+				.size() > 0) {
 			MediaPermission.signinMediaPermission();
 			CommonUtils.getdriver().findElement(MobileBy.id("signInButton")).click();
 		} else {
 			System.out.println("Signin activity is not found");
 		}
+		CommonUtils.waitForElementVisibility("//*[@class='android.widget.Switch'][@text='ON']");
 	}
 	
 	// verifying user home page navigation
@@ -75,7 +76,7 @@ public class HomepageAction {
 	}
 
 	// signout
-	public static void signOutAction() throws MalformedURLException, InterruptedException {
+	public static void signOutAction() throws MalformedURLException, InterruptedException, ParseException {
 		Thread.sleep(3000);
 		CommonUtils.waitForElementVisibility("//*[@class='android.widget.Switch']");
 		CommonUtils.SwitchStatus("startStopWorkSwitch");
@@ -88,24 +89,25 @@ public class HomepageAction {
 	}
 	
 	// signout cases
-	public static void signout_cases() throws InterruptedException, MalformedURLException {
+	public static void signout_cases() throws InterruptedException, MalformedURLException, ParseException {
 		CommonUtils.interruptSyncAndLetmeWork();
-		if (CommonUtils.getdriver().findElement(MobileBy.id("saveForm")).isDisplayed()) {
+		if (CommonUtils.getdriver().findElements(By.id("saveForm")).size() > 0) {
 			Forms.verifyFormPagesAndFill();
-		} else if (CommonUtils.getdriver().findElement(MobileBy.id("signInButton")).isDisplayed()) {
-			CommonUtils.getdriver().findElement(MobileBy.id("signInButton")).click();
+		} else if (CommonUtils.getdriver().findElements(By.id("signInButton")).size() > 0) {
+			CommonUtils.getdriver().findElement(By.id("signInButton")).click();
 			// capture reason
-			if (CommonUtils.getdriver().findElement(By.id("heading")).isDisplayed()) {
+			if (CommonUtils.getdriver().findElements(By.id("heading")).size() > 0) {
 				CommonUtils.getdriver().findElements(By.className("android.widget.Button")).get(0).click();
 			}
-			if (CommonUtils.getdriver().findElement(MobileBy.id("saveForm")).isDisplayed()) {
+			if (CommonUtils.getdriver().findElements(By.id("saveForm")).size() > 0) {
 				Forms.verifyFormPagesAndFill();
 			}
-		} else if (CommonUtils.getdriver().findElement(MobileBy.id("button1")).isDisplayed()) {
-			CommonUtils.getdriver().findElement(MobileBy.xpath("button1")).click();
+		} else if (CommonUtils.getdriver().findElements(By.xpath("//*[@text='SIGN OUT']")).size() > 0) {
+			CommonUtils.getdriver().findElement(By.xpath("//*[@text='SIGN OUT']")).click();
 		} else {
 			System.out.println("Location not found");
 		}
+		CommonUtils.waitForElementVisibility("//*[@class='android.widget.Switch'][@text='OFF']");	
 	}
 
 	//form alert signout
