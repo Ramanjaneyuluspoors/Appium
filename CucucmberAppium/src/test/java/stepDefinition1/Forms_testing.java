d104e645f30911fe0e69d37eb6aaf44c1f9b1186
unknown <ramanjaneyulu.nelampati@spoors.in>
2020-12-21 13:07:44 +0530

    added changes

---
+++ CucucmberAppium/src/test/java/stepDefinition1/Forms_testing.java
@@ -0,0 +1,170 @@
+package stepDefinition1;
+
+import java.net.MalformedURLException;
+import java.text.ParseException;
+
+import Actions.MobileActionGesture;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import modules_test.CustomerPageActions;
+import modules_test.FormAdvanceSettings;
+import modules_test.Forms;
+import utils.CommonUtils;
+
+public class Forms_testing {
+	@Given("^scroll to form$")
+	public void scroll_to_form(String form) throws MalformedURLException, InterruptedException, ParseException {
+		Forms.verifyForminHomePage(form);
+	}
+	
+	@When("^user click on the specified form$")
+	public static void user_click_on_the_specified_form(String form) {
+		MobileActionGesture.scrollTospecifiedElement("" + form + "");
+		CommonUtils.waitForElementVisibility("//*[@text='" + form + "']");
+	}
+	
+	@Then("user fill the form and save")
+	public void user_fill_the_form_and_save() throws MalformedURLException, InterruptedException, ParseException {
+		Forms.verifyFormPagesAndFill();
+		Forms.formSaveButton();
+	}
+	
+	@And("modify the form and save")
+	public void modify_the_form_and_save() throws MalformedURLException, InterruptedException, ParseException {
+		Forms.modify_form();
+//		CustomerPageActions.navigate_back();
+	}
+	
+	@And("Navigate to homepage")
+	public void Navigate_to_homepage() throws MalformedURLException, InterruptedException, ParseException {
+		CustomerPageActions.navigate_back();
+	}
+	
+	@Given("^swipe to form$")
+	public void swipe_to_form(String form) throws MalformedURLException, InterruptedException, ParseException {
+		Forms.verifyForminHomePage(form);
+	}
+
+	@When("^user click on the given formname$")
+	public static void user_click_on_the_given_formname(String form) {
+		user_click_on_the_specified_form(form);
+	}
+	
+	@Then("user fill the form and")
+	public void user_fill_the_form_and() throws MalformedURLException, InterruptedException, ParseException {
+		Forms.verifyFormPagesAndFill();
+	}
+	
+	@And("click save and new")
+	public static void click_save_and_new() throws InterruptedException {
+		Forms.form_Save_And_New();
+	}
+	
+	@Then("Discard the form")
+	public static void Discard_the_form() throws InterruptedException {
+		Forms.form_Discard();
+	}
+	
+	@Given("^scroll and click on forms$")
+	public static void scroll_and_click_on_forms(String form) throws MalformedURLException {
+		Forms.verifyForminHomePage(form);
+	}
+	
+	@When("user click on the form available in the forms ")
+	public static void user_click_on_the_form_available_in_the_forms(String form) {
+		user_click_on_the_specified_form(form);
+	}
+	
+	@Then("user insert form data")
+	public static void user_insert_form_data() throws MalformedURLException, InterruptedException, ParseException {
+		Forms.verifyFormPagesAndFill();
+	}
+	
+	@And("save form as draft")
+	public static void save_form_as_draft() throws InterruptedException {
+		Forms.form_Save_As_Draft();
+	}
+	
+	@Given("^Swipe to MinMax form$")
+	public void Swipe_to_MinMax_form(String MinMaxform) throws InterruptedException, MalformedURLException {
+		Forms.verifyForminHomePage(MinMaxform);
+	}
+	
+	@When("^user click on given form$")
+	public static void user_click_on_given_form(String form) {
+		user_click_on_the_specified_form(form);
+	}
+
+	@Then("user enters Min value {int} and Max value {int}")
+	public static void user_enters_Min_value_and_Max_value(int min, int max) throws MalformedURLException, InterruptedException {
+		FormAdvanceSettings.clickSectionInPages();
+		FormAdvanceSettings.minMaxTesting(min, max);
+	}
+	
+	@Given("^scroll to specified form$")
+	public static void scroll_to_specified_form(String form) throws MalformedURLException {
+		Forms.verifyForminHomePage(form);
+	}
+	
+	@When("^user click on given form name$")
+	public static void user_click_on_given_form_name(String fieldDepency_form) {
+		user_click_on_the_specified_form(fieldDepency_form);
+	}
+	
+	@Then("user gives the basecondition {string} and dependencyfield {string} and input is {string}")
+	public static void user_gives_the_basecondition_and_dependencyfield_and_input_is(String baseCondition, String formFieldLabel, String formFieldInput) throws MalformedURLException, InterruptedException {
+		FormAdvanceSettings.clickSectionInPages();
+		FormAdvanceSettings.fieldDependencyValueOtherFields(baseCondition, formFieldLabel, formFieldInput);
+	}
+	
+	@Given("move to specified form")
+	public void move_to_specified_form(String form) throws MalformedURLException {
+		Forms.verifyForminHomePage(form);
+	}
+	
+	@When("^user click on the given form$")
+	public static void user_click_on_the_given_form(String regularExpression_form) {
+		user_click_on_the_specified_form(regularExpression_form);
+	}
+
+	@Then("user enters the Regular Expression {string} for datatype {string}")
+	public void user_enters_the_Regular_Expression_for_datatype(String regExp, String formFieldLabel) throws MalformedURLException, InterruptedException {
+		FormAdvanceSettings.clickSectionInPages();
+		FormAdvanceSettings.regularExpressionTesting(regExp, formFieldLabel);
+	}
+	
+	@Given("^scroll to specified color form$")
+	public static void scroll_to_specified_color_form(String Form) throws MalformedURLException {
+		Forms.verifyForminHomePage(Form);
+	}
+	
+	@When("^user click on the form name$")
+	public static void user_click_on_the_form_name(String colorForm) {
+		user_click_on_the_specified_form(colorForm);
+	}
+	
+	@Then("user enters the field value {int} for datatype {string}")
+	public static void user_enters_the_field_value_for_datatype(int fieldValue, String fieldLabel) throws MalformedURLException, InterruptedException {
+		FormAdvanceSettings.clickSectionInPages();
+		FormAdvanceSettings.testing_highlighting_background_field_basedOn_fieldValue(fieldValue, fieldLabel);
+	}
+	 
+	@Given("^swipe to specified form$")
+	public static void swipe_to_specified_form(String Form) throws MalformedURLException {
+		Forms.verifyForminHomePage(Form);
+	}
+	
+	@When("^user click on the form given$")
+	public static void user_click_on_the_form_given(String errorAndWarnMessageForm) {
+		user_click_on_the_specified_form(errorAndWarnMessageForm);
+	}
+	
+	@Then("user provide the condition {string} and gives input as {string}")
+	public static void user_provide_the_condition_and_gives_input_as(String errorCondition, String inputValue) throws MalformedURLException, InterruptedException, ParseException {
+		FormAdvanceSettings.clickSectionInPages();
+		FormAdvanceSettings.Validate_Based_on_Values_in_Other_Fields(errorCondition, inputValue);
+	}
+
+}
--- CucucmberAppium/src/test/java/stepDefinition1/Sanity.java
+++ CucucmberAppium/src/test/java/stepDefinition1/Sanity.java
@@ -3,7 +3,6 @@ package stepDefinition1;
 import java.net.MalformedURLException;
 import java.text.ParseException;
 
-import Actions.CustomerPageActions;
 import Actions.HomepageAction;
 import Actions.MobileActionGesture;
 import Actions.ReceiptActions;
@@ -11,13 +10,13 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import modules_test.CustomerPageActions;
+import modules_test.Forms;
+import modules_test.RoutePlan;
+import modules_test.Work;
 import utils.CommonUtils;
-import utils.FormAdvanceSettings;
-import utils.Forms;
-import utils.RoutePlan;
-import utils.Work;
 
-public class StepsImplementation {
+public class Sanity {
  
 	@Given("Navigate to Home page")
 	public void navigate_to_Home_page() throws InterruptedException {
@@ -60,7 +59,8 @@ public class StepsImplementation {
 	public void Do_customer_activity_and_checkout(String formName) throws MalformedURLException, InterruptedException, ParseException {
 		CustomerPageActions.clickActivity(formName);
 		Forms.verifyFormPagesAndFill();
-		CustomerPageActions.checkout_in_customer_screen();
+		Forms.formSaveButton();
+		CustomerPageActions.HomepageCusCheckout();
 	}
 
 	@Given("^Scroll to route plan and click$")
@@ -83,6 +83,7 @@ public class StepsImplementation {
 	public void perform_routeactivity() throws MalformedURLException, InterruptedException, ParseException {
 		RoutePlan.performRouteActivity();
 		Forms.verifyFormPagesAndFill();
+		Forms.formSaveButton();
 	}
 
 	@Then("complete client visit do customer checkout")
@@ -132,17 +133,24 @@ public class StepsImplementation {
 		Forms.verifyForminHomePage(form);
 	}
 
-	@Then("fill form to complete")
-	public void fill_form_to_complete() throws MalformedURLException, InterruptedException, ParseException {
+	@When("^user click on specified form$")
+	public static void user_click_on_specified_form(String form) {
+		MobileActionGesture.scrollTospecifiedElement("" + form + "");
+		CommonUtils.waitForElementVisibility("//*[@text='" + form + "']");
+	}
+	
+	@Then("fill the form to complete")
+	public void fill_the_form_to_complete() throws MalformedURLException, InterruptedException, ParseException {
 		Forms.verifyFormPagesAndFill();
+		Forms.formSaveButton();
 		CommonUtils.goBackward();
+		CommonUtils.openMenu();
+		CommonUtils.clickHomeInMenubar();
 	}
 
 	@Given("Swipe to settings")
 	public void Swipe_to_settings() throws MalformedURLException, InterruptedException {
-		CommonUtils.openMenu();
-		MobileActionGesture.scrollTospecifiedElement("Settings");
-		CommonUtils.allow_bluetooth();
+		HomepageAction.perform_debug_action();
 	}
 	
 	@When("user click on send debug info")
@@ -161,58 +169,4 @@ public class StepsImplementation {
 		HomepageAction.signOutAction();
 	}
 	
-	@Given("^Swipe to MinMax form$")
-	public void Swipe_to_MinMax_form(String MinMaxform) throws InterruptedException, MalformedURLException {
-		Forms.verifyForminHomePage(MinMaxform);
-	}
-	
-	@Then("user enters Min value {int} and Max value {int}")
-	public static void user_enters_Min_value_and_Max_value(int min, int max) throws MalformedURLException, InterruptedException {
-		FormAdvanceSettings.clickSectionInPages();
-		FormAdvanceSettings.minMaxTesting(min, max);
-	}
-	
-	@Given("^scroll to specified form$")
-	public static void scroll_to_specified_form(String fieldDepency_form) throws MalformedURLException {
-		Forms.verifyForminHomePage(fieldDepency_form);
-	}
-	
-	@Then("user gives the basecondition {string} and dependencyfield {string} and input is {string}")
-	public static void user_gives_the_basecondition_and_dependencyfield_and_input_is(String baseCondition, String formFieldLabel, String formFieldInput) throws MalformedURLException, InterruptedException {
-		FormAdvanceSettings.clickSectionInPages();
-		FormAdvanceSettings.fieldDependencyValueOtherFields(baseCondition, formFieldLabel, formFieldInput);
-	}
-	
-	@Given("move to specified form")
-	public void move_to_specified_form(String regularExpression_form) throws MalformedURLException {
-		Forms.verifyForminHomePage(regularExpression_form);
-	}
-
-	@When("user enters the Regular Expression {string} for datatype {string}")
-	public void user_enters_the_Regular_Expression_for_datatype(String regExp, String formFieldLabel) throws MalformedURLException, InterruptedException {
-		FormAdvanceSettings.clickSectionInPages();
-		FormAdvanceSettings.regularExpressionTesting(regExp, formFieldLabel);
-	}
-	
-	@Given("^scroll to specified color form$")
-	public static void scroll_to_specified_color_form(String colorForm) throws MalformedURLException {
-		Forms.verifyForminHomePage(colorForm);
-	}
-	
-	@When("user enters the field value {int} for datatype {string}")
-	public static void user_enters_the_field_value_for_datatype(int fieldValue, String fieldLabel) throws MalformedURLException, InterruptedException {
-		FormAdvanceSettings.clickSectionInPages();
-		FormAdvanceSettings.testing_highlighting_background_field_basedOn_fieldValue(fieldValue, fieldLabel);
-	}
-	 
-	@Given("^swipe to specified form$")
-	public static void swipe_to_specified_form(String errorAndWarnMessageForm) throws MalformedURLException {
-		Forms.verifyForminHomePage(errorAndWarnMessageForm);
-	}
-	
-	@When("user provide the condition {string} and gives input as {string}")
-	public static void user_provide_the_condition_and_gives_input_as(String errorCondition, String inputValue) throws MalformedURLException, InterruptedException, ParseException {
-		FormAdvanceSettings.clickSectionInPages();
-		FormAdvanceSettings.Validate_Based_on_Values_in_Other_Fields(errorCondition, inputValue);
-	}
 }
