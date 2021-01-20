94797ea61536bde25d7f974a8430a1d7326a44fb
unknown <ramanjaneyulu.nelampati@spoors.in>
2021-01-20 16:52:51 +0530

    added leaves and reports implementation

---
+++ CucucmberAppium/src/test/java/stepDefinition1/Leaves_Implementation.java
@@ -0,0 +1,29 @@
+package stepDefinition1;
+
+import java.net.MalformedURLException;
+import java.text.ParseException;
+
+import Actions.MobileActionGesture;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import modules_test.Leaves;
+
+
+public class Leaves_Implementation {
+	@Given("fling to leaves card")
+	public static void fling_to_leaves_card() throws MalformedURLException, InterruptedException, ParseException {
+		MobileActionGesture.scrollTospecifiedElement("Leaves");
+	}
+	
+	@When("user want to apply leave with leave type {string}")
+	public static void user_want_to_apply_leave_with_leave_type(String leaveType) throws MalformedURLException, InterruptedException, ParseException {
+		Leaves.leave_apply(leaveType);
+	}
+	
+	@Then("user should successfully apply leave")
+	public static void user_should_successfully_apply_leave() {
+		Leaves.leave_Status();
+		Leaves.leave_cancel();
+	}
+}
---
+++ CucucmberAppium/src/test/java/stepDefinition1/ReportsImplementation.java
@@ -0,0 +1,37 @@
+package stepDefinition1;
+
+import java.net.MalformedURLException;
+import java.text.ParseException;
+
+import Actions.MobileActionGesture;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import modules_test.Reportsverification;
+
+public class ReportsImplementation {
+
+	@Given("Fling to reports card")
+	public void Fling_to_reports_card() throws MalformedURLException, InterruptedException, ParseException {
+		Thread.sleep(4000);
+		MobileActionGesture.scrollTospecifiedElement("Reports");
+		Thread.sleep(4000);
+	}
+
+	@When("user click on specified report name and validate data")
+	public void user_click_on_specified_report_name() throws InterruptedException {
+		Reportsverification.employeeActivityWiseSummaryReport();
+		Reportsverification.distanceTravelledReport();
+		Reportsverification.employeeWiseActivitySummaryReport();
+		Reportsverification.employeeSignInSignOutReport();
+		
+	}
+	
+	@Given("scroll to day plan report with specified employee {string} and validate report data")
+	public void scroll_to_day_plan_report_with_specified_employee_and_validate_report_data(String employeeName) throws InterruptedException {
+		Reportsverification.dayPlanReport(employeeName);
+		Reportsverification.validatingDayPlanReportData();
+		Reportsverification.employeeCoverageReport(employeeName);
+	}
+	
+	
+}
